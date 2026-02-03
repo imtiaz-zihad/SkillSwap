30622e2a6178ae568cd35978da519e0206218102
Imtiaz <mdimtiazzihad@gmail.com>
2026-02-03 20:19:06 +0600

    Poject Enviroment Setup Complete

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+
+const app: Application = express();
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        Message: "Skillswap - Micro Skill Exchange Platform"
+    })
+});
+
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
