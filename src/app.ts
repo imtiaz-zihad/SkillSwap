745e322eaa53546987541387d07a16dfc851af96
Imtiaz <mdimtiazzihad@gmail.com>
2026-02-03 20:56:23 +0600

    Prisma setup complete

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,29 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import notFound from './app/middlewares/notFound';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import config from "./config";
 
 const app: Application = express();
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Skillswap - Micro Skill Exchange Platform"
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Skillswap - Micro Skill Exchange Platform",
+    environment: config.node_env,
+    uptime: process.uptime().toFixed(2) + " sec",
+    timeStamp: new Date().toISOString(),
+  });
 });
-
 
 app.use(globalErrorHandler);
 
